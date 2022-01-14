e3a6228d430ac64e0af418ca3b82184022ab40e6
Irina <irina.afanassenko@gmail.com>
2022-01-14 13:53:29 +0100

    My first version of Calculator

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(sum(-25, 50));
+
+        System.out.println(mul(5, 4));
+    }
+
+
+    public static double sum(double n1, double n2) {
+        return Math.abs(n1) + Math.abs(n2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
